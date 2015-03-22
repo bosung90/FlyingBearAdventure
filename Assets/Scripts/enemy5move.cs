fb40605fa73fffa166d9e930f705d4b7763773bd
Lisa Wong <miss.lisa19@hotmail.com>
2015-03-21 22:20:05 -0700

    added paths for the enemies, shooting & bomb sounds

---
+++ Assets/Scripts/enemy5move.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemy5move : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		iTween.MoveTo (gameObject, iTween.Hash ("path", iTweenPath.GetPath ("enemy5path"), "time", 10, "easetype", iTween.EaseType.easeInOutSine, "looptype", iTween.LoopType.pingPong));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
