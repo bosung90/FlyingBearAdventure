3939aaaf1ecbc6de88b28f85a95582cfa7e5fc78
John <johnlim0521@gmail.com>
2015-03-21 23:14:00 -0700

    done quest

--- Assets/Scripts/Quest.cs
+++ Assets/Scripts/Quest.cs
@@ -4,40 +4,58 @@ using System.Collections;
 public class Quest : MonoBehaviour {
 
 	public enum questType {Quest1, Quest2, Quest3, Finish};
-	GameObject _bombPrefab, _dataPrefab;
-	Transform camera;
+	public GameObject _bombPrefab, _dataPrefab;
+	public Transform camera;
 	questType currentQ;
+	bool _hasBomb, _posted;
+	public GameObject[] _marked, _checked;
 
 
 	// Use this for initialization
 	void Start () {
 		currentQ = questType.Quest1;
+		_hasBomb = false;
+		_posted = true;
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-		GameObject[] _marked = GameObject.FindGameObjectsWithTag ("mark");
+		_marked = GameObject.FindGameObjectsWithTag("mark");
 
 
-		if (_marked == null) {
+		if (_marked.Length == 0 && _posted) {
 			currentQ = questType.Finish;
-			this.transform.position = new Vector3(0,0,0);
+			Debug.Log ("CurrentQuest : finished");
+			_posted = false;
 		}
 	
 	}
 
-	void OnCollisionEnter(Collision collision) {
+	void OnTriggerEnter(Collider collision) {
+		if(collision.gameObject.tag != "undefined") {
+			Debug.Log ("Currently touching : " + collision.gameObject.tag);
+		}
+
+		if(collision.gameObject.tag == "testing") {
+			collision.gameObject.tag = "checked";
+		}
+
 		if (currentQ == questType.Quest1 && collision.gameObject.tag == "data") {
 			currentQ = questType.Quest2;
 			Destroy (collision.gameObject);
+			Debug.Log ("CurrentQuest : quest2 entered");
 		} else if (currentQ == questType.Quest2 && collision.gameObject.tag == "comp") {
 			currentQ = questType.Quest3;
-			GameObject newData = Instantiate (_dataPrefab);
+			GameObject newData = Instantiate (_dataPrefab) as GameObject;
 			newData.transform.position = this.transform.position + camera.transform.forward;
-		} else if (currentQ == questType.Quest3 && collision.gameObject.tag == "mark") {
-			GameObject newBomb = Instantiate (_bombPrefab);
+			Debug.Log ("CurrentQuest : quest3 entered");
+		} else if (currentQ == questType.Quest3 && collision.gameObject.tag == "bomb") {
+			_hasBomb = true;
+			Destroy (collision.gameObject);
+		} else if (currentQ == questType.Quest3 && collision.gameObject.tag == "mark" && _hasBomb) {
+			GameObject newBomb = Instantiate (_bombPrefab) as GameObject;
 			Vector3 _pos = collision.gameObject.transform.position;
 			newBomb.transform.position = new Vector3 (_pos.x, _pos.y + 0.1f, _pos.z);
 			collision.gameObject.tag = "marked";
