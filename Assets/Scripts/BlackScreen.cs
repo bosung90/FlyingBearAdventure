ad56a5b0837bdf0a368d99920a928e059d20abca
John <johnlim0521@gmail.com>
2015-03-22 20:46:48 -0700

    end scene

---
+++ Assets/Scripts/BlackScreen.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class BlackScreen : MonoBehaviour {
+
+	Image screen;
+
+	// Use this for initialization
+	void Start () {
+
+		screen = GetComponent<Image> ();
+		screen.enabled = false;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Quest.currentQ == Quest.questType.Finish || Quest.currentQ == Quest.questType.GameOver) {
+			screen.enabled = true;
+		}
+	
+	}
+}
---
+++ Assets/Scripts/TextStatus.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TextStatus : MonoBehaviour {
+
+	Text msg;
+
+	// Use this for initialization
+	void Start () {
+
+		msg = GetComponent<Text> ();
+		msg.text = "";
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Quest.currentQ == Quest.questType.Finish) {
+			msg.text = "Mission Accomplished";
+		} else if (Quest.currentQ == Quest.questType.GameOver) {
+			msg.text = "YOU DIED, GAME OVER";
+		}
+
+	
+	}
+}
