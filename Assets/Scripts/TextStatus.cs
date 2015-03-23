fda7523a6c84f1329662bccde35492bcf76167d3
John <johnlim0521@gmail.com>
2015-03-22 20:52:56 -0700

    no message

--- Assets/Scripts/TextStatus.cs
+++ Assets/Scripts/TextStatus.cs
@@ -9,16 +9,21 @@ public class TextStatus : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 
-		msg = GetComponent<Text> ();
-		msg.text = "";
+			msg = GetComponent<Text> ();
+		msg.enabled = false;
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+
+
 		if (Quest.currentQ == Quest.questType.Finish) {
+			msg.enabled = true;
 			msg.text = "Mission Accomplished";
 		} else if (Quest.currentQ == Quest.questType.GameOver) {
+			msg.enabled = true;
 			msg.text = "YOU DIED, GAME OVER";
 		}
 
